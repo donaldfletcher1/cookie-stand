926c6e8a04a67e0db0cfc0ee2639c237b3f2c199
Donald Fletcher <donaldfletcher11@outlook.com>
2023-02-22 21:00:53 -0800

    objects

---
+++ app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let seattle = {
+ minCustomers: 23,
+ maxCustomers: 65,
+ averageCookies: 6.3,
+ function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+}
+getRandomArbitrary()
+
+
+
+
+
+
+
+let tokyo = {
+minCustomers: 3,
+maxCustomers: 24,
+averageCookies: 1.2,
+}
+
+
+
+
+let dubai ={
+minCustomers: 11,
+maxCustomers: 38,
+averageCookie: 3.7
+}
+
+
+
+
+let paris ={
+minCustomers: 20,
+maxCustomers: 38,
+averageCookie: 2.3    
+}
+
+
+
+
+
+let lima ={
+minCustomers: 2,
+maxCustomers: 16,
+averageCookie: 4.6,    
+}
+
